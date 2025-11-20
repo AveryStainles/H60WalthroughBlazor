cf3598ff0be18d2228b33f9f54bb79ff3b57eb00
Avery <Avery@DESKTOP-94VLUDF>
2025-11-20 11:33:59 -0500

    Completed Blazor walthrough

---
+++ OnlineShop/States/CartState.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShop.States;
+using OnlineShop.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+public class CartState(HttpClient _httpClient)
+{
+    public List<Product> SelectedItems { get; set; } = new();
+
+    public async Task AddProductToCartAsync(Guid productId)
+    {
+        if (!SelectedItems.Any(p => p.Id == productId) is false)
+            return;
+
+        var products = await _httpClient.GetFromJsonAsync<List<Product>>("sample-data/books.json") ?? new();
+        var product = products.First(p => p.Id == productId);
+        SelectedItems.Add(product);
+    }
+}
